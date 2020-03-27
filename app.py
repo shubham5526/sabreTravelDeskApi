677b2b3284396dc7a6b2e5cc2f2de10152208c3d
shubham5526 <41041596+shubham5526@users.noreply.github.com>
2020-03-27 18:21:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-from flask import Flask
-from flask_restful import Resource, reqparse, Api
+import flask
+from flask import request, jsonify
 import requests
 
-app = Flask(__name__)
-api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-
-from base import Movies, db
-db.init_app(app)
-app.app_context().push()
-db.create_all()
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
 API_ENDPOINT = "https://api-crt.cert.havail.sabre.com/v1/offers/shop"
+
 API_KEY = "T1RLAQKdwHQFmCgiuM/R7mCaG+6DXV9TYBBHHar0enUCDHVn6zBrxvn6AADAM7CWdWstzKO0VXPYRxhOAtGKWudnKSmGO0GwbWbc5wA/yg0lkHu6daO9cV6Af5Zhjk4jjH+7Som1Ycsukkr3gIF41qQEfnfE8eV+ee2O4KzXz1nvdOwZrcb6NXmWuH37f6c1G915GfVqVL1hj2ATIU4lXIi8QLkqkRO7nFcjZQbPFL2p0r7OitP+CsoQcqCJIHZC8sj350N8wmGxr60r0kX+epEWvJ97VkXVX+8Fu0n6FS9KbnRSN4/pmVJq9+2/"
 hed = {'Authorization': 'Bearer ' + API_KEY}
 data = {
@@ -22,7 +15,7 @@ data = {
             {
                 "DepartureDateTime": "2020-06-21T00:00:00",
                 "DestinationLocation": {
-                    "LocationCode": "LAX"
+                    "LocationCode": "SFO"
                 },
                 "OriginLocation": {
                     "LocationCode": "NYC"
@@ -35,7 +28,7 @@ data = {
                     "LocationCode": "NYC"
                 },
                 "OriginLocation": {
-                    "LocationCode": "LAX"
+                    "LocationCode": "SFO"
                 },
                 "RPH": "1"
             }
@@ -90,56 +83,33 @@ data = {
     }
 }
 
-class Movies_List(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('director', type=str, required=False, help='Director of the movie')
-    parser.add_argument('genre', type=str, required=False, help='Genre of the movie')
-    parser.add_argument('collection', type=int, required=True, help='Gross collection of the movie')
-    
-    def get(self, movie):
-        r = requests.post(url=API_ENDPOINT, json=data, headers=hed)
-        return str(r.content)
-        
-    
-    def post(self, movie):
-        if Movies.find_by_title(movie):
-            return {' Message': 'Movie with the  title {} already exists'.format(movie)}
-        args = Movies_List.parser.parse_args()
-        item = Movies(movie, args['director'], args['genre'], args['collection'])
-        item.save_to()
-        return item.json()
-        
-    def put(self, movie):
-        args = Movies_List.parser.parse_args()
-        item = Movies.find_by_title(movie)
-        if item:
-            item.collection = args['collection']
-            item.save_to()
-            return {'Movie': item.json()}
-        item = Movies(movie, args['director'], args['genre'], args['collection'])
-        item.save_to()
-        return item.json()
-            
-    def delete(self, movie):
-        item  = Movies.find_by_title(movie)
-        if item:
-            item.delete_()
-            return {'Message': '{} has been deleted from records'.format(movie)}
-        return {'Message': '{} is already not on the list'.format()}
-    
-class All_Movies(Resource):
-    def get(self):
-         r = requests.post(url=API_ENDPOINT, json=data, headers=hed)
-        return str(r.content)
-    
-api.add_resource(All_Movies, '/')
-api.add_resource(Movies_List, '/<string:movie>')
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
 
 @app.route('/api/v1/resources/bargainer/all', methods=['GET'])
 def api_all():
-r = requests.post(url=API_ENDPOINT, json=data, headers=hed)
-return str(r.content)
+    r = requests.post(url=API_ENDPOINT, json=data, headers=hed)
+    return str(r.content)
 
-if __name__=='__main__':
-    
-app.run(debug=True)
+app.run()
