913bc6764f54cca8382d762fb1848b3324927664
shubham5526 <41041596+shubham5526@users.noreply.github.com>
2020-03-31 14:52:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-import flask
-from flask import request, jsonify
-
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-
-
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-
-app.run()
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+if __name__ == '__main__':
+    app.run()
