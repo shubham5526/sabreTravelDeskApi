c05425be79606b1296ee6b6b52a98bbb2e1afe57
shubham5526 <41041596+shubham5526@users.noreply.github.com>
2020-03-31 18:11:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-app.run()
+if __name__ == "__main__":
+    app.run()
