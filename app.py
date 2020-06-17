738c39f3f2ca5293c24748f23d5cbb05f04a27e5
shubham5526 <41041596+shubham5526@users.noreply.github.com>
2020-06-17 16:31:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1492,6 +1492,13 @@ def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 
+@app.route('/api/v1/resources/authenticate', methods=['POST'])
+def clientauthentication():
+    print(request.headers['Authorization'])
+    objCommonHelper = CommonHelper.PostgressController()
+    return objCommonHelper.clientAuthentication('', '')
+
+
 @app.route('/api/v1/resources/getairports', methods=['GET'])
 def getAirports():
     searchTerm = request.args['searchTerm']
